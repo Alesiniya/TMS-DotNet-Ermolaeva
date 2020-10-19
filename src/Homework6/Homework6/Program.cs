b36407604a15ab92c5812eb3da94b88cfb419139
Ваше имя <alesya-13@mail.ru>
2020-10-20 00:44:29 +0300

    feat: the solution homework 6

---
+++ src/Homework6/Homework6/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+
+namespace Homework6
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Atm atm = new Atm();
+            atm.BalanceHandler += GetInfo;
+            atm.Put(100.00);
+            atm.Get(15.00);
+            Console.ReadKey();
+        }
+
+        public static void GetInfo(double amount, string operation)
+        {
+            Console.WriteLine($"Operation {operation}. Current balance: {amount}");
+        }
+    }
+}
