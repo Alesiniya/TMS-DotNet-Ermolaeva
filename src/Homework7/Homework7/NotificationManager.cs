e2421b613decbb3dc8176f69b620bd9c1a286301
Alesiniya <alesya-13@mail.ru>
2020-12-02 02:14:33 +0300

    feat: add the solution Homework 7

---
+++ src/Homework7/Homework7/NotificationManager.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Homework7
+{
+    public static class NotificationManager
+    {
+        public static void Show(string message, DateTime dateTime)
+        {
+            Console.WriteLine($"{message} {dateTime}");
+        }
+    }
+}
---
+++ src/Homework7/Homework7/User.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Homework7
+{
+    public class User
+    {
+        public User()
+        {
+        }
+
+        public User(
+            string fullName,
+            double weight,
+            int height,
+            int age
+            )
+        {
+
+            FullName = fullName;
+            Weight = weight;
+            Height = height;
+            Age = age;
+            ;
+        }
+
+        public string FullName { get; set; }
+
+        public double Weight { get; set; }
+
+        public int Height { get; set; }
+
+        public int Age { get; set; }
+
+
+        public List<Run> Runs { get; set; } = new List<Run>();
+
+        public List<Exercise> Exercises { get; set; } = new List<Exercise>();
+    }
+}
